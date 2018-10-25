45a3fee1fc96029ad12d17b1e5087b14a3926927
DaniAlvrez <6052.joan23@fje.edu>
2018-10-25 16:53:56 +0200

    Recursos V2
    Version 2 de recursos

--- PaginaWeb/assets/php/recursos.php
+++ PaginaWeb/assets/php/recursos.php
@@ -10,51 +10,50 @@
             $padre=0;
             }
             */
-            $disponible=mysqli_query($link, "SELECT * FROM tbl_recurso WHERE disponibilidad_recurso='si' ORDER BY id_recurso");
-           // $nodisponible=mysqli_query($link, "SELECT * FROM categoria WHERE ver_cat=1 ORDER BY cat_id");
-           // $incidencia=mysqli_query($link, "SELECT * FROM categoria WHERE ver_cat=1 ORDER BY cat_id");
-            //$query = mysqli_query($link, "SELECT * FROM categoria ORDER BY cat_id");
-           echo "Reqursos Disponibles<br>";
-            if(mysqli_num_rows($disponible)>0){
-                while($disponible = mysqli_fetch_array($disponible)){
-                    echo "Categoria: $disponible[nombre_recurso] [$disponible[tipo_recurso]]: ";
-                  /*  echo "<a href='modificar_categoria.php?cat_id=$categoria[cat_id]'><i class='fas fa-pencil-alt'></i></a>";
-                    echo "<a href='ver.proc.php?cat_id=$categoria[cat_id]&ver_cat=$categoria[ver_cat]'><i class='far fa-eye-slash'></i></a>";
-                    echo "<a href='eliminar.proc.php?cat_id=$categoria[cat_id]'><i class='fas fa-times'></i></a>";
-                    echo "<br>";*/
-
-                    
-                    
-                }
-            } /*
-             echo "<br>Recursos reservados<br>";
-                if(mysqli_num_rows($nodisponible)>0){
-                while($categoria = mysqli_fetch_array($nodisponible)){
-                    echo "Categoria: $categoria[cat_id]  [$categoria[cat_nombre]]: ";
-                    echo "<a href='modificar_categoria.php?cat_id=$categoria[cat_id]'><i class='fas fa-pencil-alt'></i></i></a>";
-                    echo "<a href='ver.proc.php?cat_id=$categoria[cat_id]&ver_cat=$categoria[ver_cat]'><i class='far fa-eye'></i></i></a>";
-                    echo "<a href='eliminar.proc.php?cat_id=$categoria[cat_id]'><i class='fas fa-times'></i></a>";
-                    echo "<br>";
-
-                    
-                    
-                }
-            } 
-            echo "<br>Recursos en servicio técnico<br>";
-                if(mysqli_num_rows($incidencia)>0){
-                while($categoria = mysqli_fetch_array($incidencia)){
-                    echo "Incidencia: $categoria[cat_id]  [$categoria[cat_nombre]]: ";
-                    echo "<a href='modificar_categoria.php?cat_id=$categoria[cat_id]'><i class='fas fa-pencil-alt'></i></i></a>";
-                    echo "<a href='ver.proc.php?cat_id=$categoria[cat_id]&ver_cat=$categoria[ver_cat]'><i class='far fa-eye'></i></i></a>";
-                    echo "<a href='eliminar.proc.php?cat_id=$categoria[cat_id]'><i class='fas fa-times'></i></a>";
-                    echo "<br>";
-
-                    
-                    
+            //Filtro
+            $q="SELECT * FROM `tbl_recurso` ORDER BY `tbl_recurso`.`disponibilidad_recurso` ASC";
+            $q_filtros=mysqli_query($link, $q);
+            echo "Categoría: <select name='categoria'>"; 
+            echo "<option value='todos'>Todos</option>";
+            echo "<option value='si'>Disponible</option>";
+            echo "<option value='no'>No disponible</option>";
+            echo "<option value='incidencia'>Incidencia</option>";
+            echo "</select><br/><br/>";
+            
+            //SQLS
+            $disp=mysqli_query($link, "SELECT * FROM tbl_recurso WHERE disponibilidad_recurso='si' ORDER BY id_recurso");
+            $nodisponible=mysqli_query($link, "SELECT * FROM tbl_recurso WHERE disponibilidad_recurso='no' ORDER BY id_recurso");
+            $tiempo=mysqli_query($link, "SELECT * FROM `tbl_reserva` ORDER BY `tbl_reserva`.`tiempoEstimado_reserva` DESC");
+            $incidencia=mysqli_query($link, "SELECT * FROM tbl_recurso WHERE disponibilidad_recurso='incidencia' ORDER BY id_recurso");
+            $motivo=mysqli_query($link, "SELECT * FROM `tbl_incidencia` ORDER BY `tbl_incidencia`.`descripcion_incidencia` ASC");
+            //Consultas a mostrar
+           echo "Recursos Disponibles<br>";
+            if(mysqli_num_rows($disp)>0){
+                while($disponible = mysqli_fetch_array($disp)){
+                    echo "$disponible[nombre_recurso]. Tipo: [$disponible[tipo_recurso]] <br>";  
                 }
             }
-*/
-    ?>
+            echo "<br><br>Recursos Ocupados<br>";
+            if(mysqli_num_rows($nodisponible)>0){
+                while($disponible = mysqli_fetch_array($nodisponible)){
+                    $tempo = mysqli_fetch_array($tiempo);
+                    $nrecurso=$disponible['nombre_recurso'];
+                    $tiempoaprox= $tempo['tiempoEstimado_reserva'];
+                    echo "Recurso reservado: ".$nrecurso."<br>";
+                    echo "Tiempo reserva (Aprox): ".$tiempoaprox."<br><br>";   
+                }
+            }
+            echo "<br><br>Incidencias<br>";
+            if(mysqli_num_rows($incidencia)>0){
+                while($disponible = mysqli_fetch_array($incidencia)){
+                    $mot = mysqli_fetch_array($motivo);
+                    $nrecurso=$disponible['nombre_recurso'];
+                    $motivo_recurso= $mot['descripcion_incidencia'];
+                    echo "Nombre recurso: ".$nrecurso."<br>";
+                     echo "Motivo Incidencia: ".$motivo_recurso."<br><br>"; 
+                }
+            }
+?>
          <!--   <br>
             <a href="index.php?insertaRecurso=si">Insertar Recursos <i class="fas fa-plus-square"></i></a><br> -->
              
